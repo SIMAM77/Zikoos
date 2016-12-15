960f8478926f0e2a8a7d4c4a993e6c741e8ab9ab
benImran <benchaa.said-medjahed@hetic.net>
2016-12-15 21:37:49 +0100

    back-end et intégration front-end

---
+++ header.php
@@ -0,0 +1,25 @@
+
+	<body>
+		<header>
+			<figure>
+				<a href="index.php"><img class="logo" src="image/zikoos.png" alt=""></a>
+			</figure>
+			<nav>
+				<ul class="header_ul">
+					<li class="groupes"><a href="groupes2.php">Groupes</a></li>
+					<li class="contribution"><a href="contributions.php">Contributions</a></li>
+				</ul>
+			</nav>
+			<section class="clearfix">
+				<div>
+
+					<form action="groupes2.php" method="post">
+						<input class="recherche" placeholder="Rechercher..." name="search" >
+						<div class="buttonheader">
+					<input class="valider" type="submit" id="valider" name="valider"  value="valider">
+				</div>
+					</form>
+					<a href="#"><img class="imgheader" src="image/avatar.png"></a>
+			</section>
+		</header>
+	</body>
